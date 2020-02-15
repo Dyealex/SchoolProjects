6e15be4e3d7c6f4f2a26d97c9916390c9d9e5446
Dyealex <60356813+Dyealex@users.noreply.github.com>
2020-02-15 06:00:28 -0500

    added text

---
+++ calculatorQuiz.js
@@ -0,0 +1,18 @@
+// that darn calculator...
+const currentResult = document.getElementById("currentResult")
+function div() {
+    let ans = Number (buttonFirst) % Number (buttonSecond)
+
+    currentResult.innerHTML= ans
+let buttonFirst = document.getElementById("buttonFirst").parseFloat=value=ans
+let buttonSecond = document.getElementById("buttonSecond").parseFloat=value=ans
+
+}
+
+
+
+
+
+
+
+
