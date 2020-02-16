0ef28ef8321653be61216b951989819d0ba43c47
Dyealex <60356813+Dyealex@users.noreply.github.com>
2020-02-16 04:41:44 -0500

    added text

--- calculatorQuiz.js
+++ calculatorQuiz.js
@@ -4,8 +4,8 @@ function div() {
     let buttonFirst = document.getElementById("buttonFirst").value;
     let buttonSecond = document.getElementById("buttonSecond").value;
 // That worked but I can't get the decimal points in.    
-
-    let ans = Number (buttonFirst) / Number (buttonSecond)
+// had a % sign instead of the forward slash
+    let ans = Number (buttonFirst) /  Number (buttonSecond)
 // these are the input buttons
     currentResult.innerHTML = ans;
 
