ebbde92ab63fb60d97889050e2ea1d58acff72cf
Diane Alexander <dyealex@hughes.netgit config --global user.name Dyealexgit add .git configcd ../git config --global user.email dyealex@hughes.netgit config user email dyealex@hughes.net>
2020-02-21 07:06:31 -0500

    added text

--- countdowntimerexercise.js
+++ countdowntimerexercise.js
@@ -1,22 +1,25 @@
 // This is totally broken. Now I can't get it to do anything and can't find the original page that I got it from!!!!! And it is now 2:49!!!
 // found another source. Could never find the original one...
-
-
-function printNumbers(from, to) {
+// Now it is a count down timer!
+function printNumbers(to, from) {
+// function printNumbers(to, from) {original
+    // let current = from;original
     let current = from;
   
     setTimeout(function go() {
     //   alert(current);
-      if (current < to) {
+      // if (current < to) {original
+      if (current > to) {
         setTimeout(go, 1500);
-        document.getElementById("stupidCrap").innerHTML=current++;
+        document.getElementById("stupidCrap").innerHTML=current--;
       }
     //   current++;
     }, 1500);
   }
 //   changed plus plus to plus 1..and it stopped at 1
   // usage:
-  printNumbers(0,  11);
+  printNumbers(0, 11);
+  // changed to print 11 to zero
 //   changed 1 to zero to see what happens
 // changed 10 to 11
 
