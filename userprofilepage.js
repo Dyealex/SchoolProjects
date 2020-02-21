2fa527897e807bb27e9e1ef366606b565d879588
Dyealex <dyealex@hughes.net>
2020-02-21 12:38:20 -0500

    added text

---
+++ userprofilepage.js
@@ -0,0 +1,17 @@
+document. getElementById("userName").style.fontFamily = "sans-serif"
+default, leave css style
+round , change border radius to 50%
+larger, change width to 50vw
+
+can check "checked' status of radio buttons with var x = document.getElementById("myRadio).checked;
+
+
+
+function changePicture() { 
+var defaulted = document.getElementById("document).checked;
+var round = document.getElementById("round").checked;
+var larger = document.getElementById("larger").checked;
+ 
+ if (defaulted)
+
+}
