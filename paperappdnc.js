c6628b9103488f62dd141b698768e4d6c52c5b65
Dyealex <dyealex@hughes.net>
2020-02-21 10:43:53 -0500

    added text

--- paperappdnc.js
+++ paperappdnc.js
@@ -1,4 +1,19 @@
 // paper order app, divide and concur javascript file
-console.log("quantity")
-console.log("finSize")
-console.log("Selection")
+let quantity ="userInput1";
+let numOfSigs="userInput2";
+let pressSheets = ""
+function sum (userInput1, userInput2) {
+    document.addEventListener(onclick).value
+
+
+}
+
+
+
+// console.log(userInput)
+// console.log("finSize")
+// console.log("Selection")
+
+// function sum(x, y) {
+//     return x + y;
+//   }
