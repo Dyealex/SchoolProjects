f758f09c9e13d07c172ac3cd0236430c1bc943f5
Dyealex <dyealex@hughes.net>
2020-02-24 15:52:55 -0500

    added text

--- week 5/javatest.js
+++ week 5/javatest.js
@@ -1,5 +1,9 @@
 // cmd exercise
 
+const nameJim = require('./secondtest.js');
+
+console.log(`User: ${nameJim.alertJim("Diane")}`);
+
 function addText () {
     return "Hello World!"
 }
---
+++ week 5/secondtest.js
@@ -0,0 +1,24 @@
+ function greetings () {
+     return("It is a cruel world!")
+ }
+console.log("It is a cruel world!")
+
+// console.log ("It is a cruel world!")
+ 
+// 
+// function sayHi(user) {
+//     return `Hello, ${user}!`;
+//   }
+// function add (){
+//  return  9 + 25
+
+// }
+// console.log(add)
+
+const alertJim = (name) => {
+    return (name);
+  };
+  
+  exports.alertJim = alertJim;
+
+//   Faruk helped me with this. I will try to improve on it later.
