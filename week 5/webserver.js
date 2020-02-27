a902f76f1082f812061edf04d685782dc3c4dd5c
Diane Alexander <dyealex@hughes.netgit config --global user.name Dyealexgit add .git configcd ../git config --global user.email dyealex@hughes.netgit config user email dyealex@hughes.net>
2020-02-27 17:44:46 -0500

    added text

--- week 5/webserver.js
+++ week 5/webserver.js
@@ -7,8 +7,10 @@ const port = 8080;
 // let write1 = ("Hello World!!!")
 const server = http.createServer (function (req, res) {
     res.writeHead(200,{'content-type': 'text/html'});
-        res.write("Hello World!!!")
+        res.write("<h1>Hello World!!!</h1>")
         res.write("Call back hell can't be any worse than this!! I will try later to figure out how to get the h1 tag around this." );
+        // document.write("<h1>Hello World!</h1>")
+        // document.writeln("<h1>Hello World!</h1>")
         // document.getElementById(write1).innerHTML=style="h1"
     // res.write.document.getElementById("").textInput=style="h1"
         res.end();   
@@ -17,6 +19,7 @@ server.listen(port, function(error) {
 if (error) {
     console.log('something went wrong', error)
 } else {
+    // document.write("<h1>Hello World!</h1>")
     console.log('this totally sucks and I have no idea what the hell is happening on ' + port)
 
 }
