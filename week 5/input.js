87025ed2fea063b635b5c50c6149564038290539
Dyealex <dyealex@hughes.net>
2020-02-28 10:28:42 -0500

    added text

--- week 5/input.js
+++ week 5/input.js
@@ -1,18 +1,41 @@
 
 
-const rl = requier('readline').createInterface(process.stdin. process.stdout);
+// const rl = requier('readline').createInterface(process.stdin. process.stdout);
+
+// module.exports = {
+//     input(prompt) {
+//         const promise = new Promise(()=> {
+//         rl.setPrompt(prompt);
+//         rl.prompt();
+//         rl.on('line', userInput => {
+//             input=userInput;
+//             resolve(input);
+// this.input=userInputj;
+
+//         })
+//     }
+
+// }
+
+const rl = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+});
 
 module.exports = {
-    input(prompt) {
-        const promise = new Promise(()=> {
+    prompt(prompt) {
         rl.setPrompt(prompt);
         rl.prompt();
-        rl.on('line', userInput => {
-            input=userInput;
-            resolve(input);
-this.input=userInputj;
-
+    },
+    in(cb) {
+        rl.on('line', data => {
+            if(data.trim() == 'exit') {
+                rl.close();
+            } else {
+                cb(data);
+            }
         })
     }
-
 }
+// solution by Harcourt
---
+++ week 5/main.js
@@ -0,0 +1,8 @@
+const input = require('./input.js');
+const output = require('./output.js');
+
+console.log(`Type 'exit' @ anytime to exit\n\n`);
+input.prompt(`What is your name?\n`);
+input.in(output.out);
+
+// solution by Harcourt
