d05eae7d4612584127bcd4a23c52bf2fc7f72c8d
Dyealex <dyealex@hughes.net>
2020-02-28 15:45:09 -0500

    added text

--- week 5/input.js
+++ week 5/input.js
@@ -1,22 +1,5 @@
 
 
-// const rl = requier('readline').createInterface(process.stdin. process.stdout);
-
-// module.exports = {
-//     input(prompt) {
-//         const promise = new Promise(()=> {
-//         rl.setPrompt(prompt);
-//         rl.prompt();
-//         rl.on('line', userInput => {
-//             input=userInput;
-//             resolve(input);
-// this.input=userInputj;
-
-//         })
-//     }
-
-// }
-
 const rl = require('readline').createInterface({
     input: process.stdin,
     output: process.stdout,
---
+++ week 5/urlrequest.js
@@ -0,0 +1,43 @@
+
+
+
+
+
+
+
+var http = require('http'); 
+var urlRequest = new request('fs'); 
+var myURL = "http://https://github.com/Dyealex/SchoolProjects/blob/master/GItEducation.html";
+const url = require('https://github.com/Dyealex/SchoolProjects/blob/master/GItEducation.html')
+
+module.exports = http.createServer()((req, res)  => {
+
+var service = require('./GItEducation.html');
+})
+// const reqUrl = url.parse(req.url, true);
+
+// if (reqUrl.pathname== '/index.html'&& req.method ==='GET'){
+// req.method + ' Endpoint: ' + reqUrl.pathname;
+// }
+// });
+// http.createServer(function(req,res){
+// var filePath = "." + requestAnimationFrame.url;
+// if (filePath =='./') {
+//   res.writeHead("200"); 
+//   filePath= './index.html';
+// }
+ 
+// var extname = "index.html"('./index.html')
+// //  
+
+   
+
+// }); 
+
+// server.listen(8080); 
+
+// console.log("server is listening on port 8080..."); 
+
+// var parsedURL = url.parse(req.url); 
+// //   var parsedPathname = parsedURL.pathname; 
+// // var path = require ('path')
