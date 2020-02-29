b4e3dce0ca72fad08a75de32aa4d11b2971f3c59
Dyealex <60356813+Dyealex@users.noreply.github.com>
2020-02-29 03:26:19 -0500

    added text

---
+++ week 5/demo_fileserver.js
@@ -0,0 +1,20 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+  var q = url.parse(req.url, true);
+  var filename = "about.html" + q.pathname;
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    } 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080);
+console.log("Hello world!")
+
+// yes, this was copy/pasted from w3 schools to get it to work. 
