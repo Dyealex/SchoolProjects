f432c5d8c44737d331c1eca24f0cf433bfb5ffc2
Dyealex <dyealex@hughes.netgit config --global user.name Dyealexgit add .git configcd ../git config --global user.email dyealex@hughes.netgit config user email dyealex@hughes.net>
2020-03-13 18:08:30 -0400

    added text

---
+++ week 1/datatypeexercise.js
@@ -0,0 +1,16 @@
+
+// Trying to complete ALL assignments
+
+
+var a = "Hello from ";
+var b = "Girard, Georia";
+var c = 50;
+var d = 3.14;
+function multi () {
+    return  c * d;
+}
+console.log(c * d)
+
+
+alert ("Hello World" + " keep your virus away!");
+console.log('Hello World' + ' keep your virus away!')
