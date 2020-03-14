f81f99cc83afaade5eb924caee0ea88b4b5e1e43
Dyealex <60356813+Dyealex@users.noreply.github.com>
2020-03-14 18:22:59 -0400

    addd text

---
+++ week8/eventlistenertest.js
@@ -0,0 +1,8 @@
+
+// document.addEventListener("click", addToCart);
+
+
+var menuItems =['Big Salad', 'Small Salad', 'Hamburger Combo', 'Cheeseburger Combo', 'Large Drink', 'Small Drink', 'Chocolate Milkshake']
+
+
+document.addEventListener(click).value ="menuItem";
