488c021f1d7f3d840f98756f388901d6fb022e73
Dyealex <60356813+Dyealex@users.noreply.github.com>
2020-03-15 13:21:27 -0400

    added text

--- week8/eventlistenertest.js
+++ week8/eventlistenertest.js
@@ -1,8 +1,31 @@
 
 // document.addEventListener("click", addToCart);
+var removeCartItemButtons = document.getElementsByClassName('btn-danger')
+console.log(removeCartItemButtons)
+for (var i = 0; i < removeCartItemButtons.length; i++) {
+    var button = removeCartItemButtons[i]
+    button.addEventListener('click', function(event) {
+        var buttonClicked = event.target
+        buttonClicked.parentElement.parentElement.remove()
+        updateCartTotal()
+    })
+}
+function updateCartTotal() {
+    var cartItemContainer=document.getElementsByClassName('cart-items')[0]
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row')
+    for (var i = 0; i < cartRows.length; i++) {
+    var cartRows = cartRows[i]
+    var priceElement = cartRows.getElementsByClassName('cart-price')[0]
+    var quantityElement = cartRows.getElementsByClassName('cart-quantity-input')[0]
+    var price = parseFloat(priceElement.innerText.replace('$',''))
+    console.log(price)
+}
+}
 
+// the array will log into console
+var itemsOrdered =['Big Salad', 'Small Salad', 'Hamburger Combo', 'Cheeseburger Combo', 'Large Drink', 'Small Drink', 'Chocolate Milkshake']
 
-var menuItems =['Big Salad', 'Small Salad', 'Hamburger Combo', 'Cheeseburger Combo', 'Large Drink', 'Small Drink', 'Chocolate Milkshake']
-
-
-document.addEventListener(click).value ="menuItem";
+//    button.addEventListener('click', function() {
+//        console.log('clicked');
+//    })
+console.log("here!")
