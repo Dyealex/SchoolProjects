161bb7cc39b96903cd5b41439cfb6b28b78a84b1
Dyealex <dyealex@hughes.net>
2020-03-16 15:45:31 -0400

    added text

---
+++ week8/carttest.js
@@ -0,0 +1,39 @@
+
+
+ var itemsOrdered = []
+ var item1 ={name:'Big Salad', cost: 10, ordered:0};
+ var item2= {name:'Small Salad', cost: 9, ordered:0};
+ var item3= {name: 'Hamburger Combo', cost:8, ordered:0};
+ var item4 ={name:'Cheeseburger Combo', cost:9, ordered:0};
+ var item5= {name:'Large Drink', cost:3, ordered:0};
+ var item6= {name:'Small Drink', cost:2, ordered:0};
+ var item7= {name:'Chocolate Milkshake', cost:4, ordered:0};
+ 
+function calculatePrice () {
+    console.log("calculatePrice clicked!!!!");
+}
+// works!!!
+
+//  itemsOrdered.push(item1, item2, item3, item4, item5, item6, item7)
+//  for (var i = 0; i < itemsOrdered.length; i++) {
+//      console.log(itemsOrdered[i]);
+//  }
+ // console.log(itemsOrdered)
+// var cart =[];
+
+// addItemsToCart(name, price, count)
+// function addItemsToCart (){
+//     var 
+// }
+
+
+
+// removeItemsFromCart(name)
+
+// removeItemsFromCartAll(removes all items from cart)
+
+// clearCart()
+
+//countCart()-returns total count
+
+//cartTotal()-return total cost
