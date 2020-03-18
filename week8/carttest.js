c7792ff6f61f0d9bc3c8e08da5f0bb10edad2a41
Dyealex <60356813+Dyealex@users.noreply.github.com>
2020-03-18 07:21:52 -0400

    added text

--- week8/carttest.js
+++ week8/carttest.js
@@ -9,40 +9,53 @@
  var item6= {name:'Small Drink', cost:2, ordered:0};
  var item7= {name:'Chocolate Milkshake', cost:4, ordered:0};
  
+ var inputElement= document.addEventListener('click', inputElement);
+//  var inputText = inputElement;
+      // removing value from end....
+      // var quantity = parseInt(inputText);
 function calculatePrice () {
-      console.log("calculatePrice clicked!!!!");
+      // console.log("calculatePrice clicked!!!!");
          // console.log("type", typeof calculatePrice());
          // ^^^^oh that was bad news!!!!
          // This works...returns calculate price in console...
          // going back and commenting out all lines and uncommenting one by one....again!
    
-   var inputElement= document.getElementById("item1Quantity");
-          // console.log("type", typeof "item1Quantity")string
+   // var inputElement= document.getElementById("item1Quantity");
+   // var inputElement= document.addEventListener('click', inputElement);
+         // and now...I will try to change this to event listener moved line up to above function to see what that changes.
+         // console.log("type", typeof "item1Quantity")string
          // console.log(inputElement);
          // This works...returns calcPrice and<input id="item1Quantity" type="text" value="0"> and also returns "undefined" in the console if I just type it in..
 
-   console.log("type", typeof inputElement)
-         //^^^This works...returns calcPrice and<input id="item1Quantity" type="text" value="0" and type string...>
+   // console.log("type", typeof inputElement)
+         //^^^This works...returns calcPrice and<input id="item1Quantity" type="text" value="0" and type string...> still works to here
   
-   var inputText = inputElement.value;
+   var inputText = inputElement
+         // ^^^moving this line to up above function....
          //This line defines inputText on next line^^^this shit will drive you stark raving mad. This would not work because i hit 'enter' on the value and it had a capitol v instead of lower case!!!!
 
-   console.log("inputText", inputText);
+   // console.log("inputText", inputText);
          // console.log("type", typeof inputText)
          //^^^This works...returns calcPrice and<input id="item1Quantity" type="text" value="0" and type string, plus input text 0 or what is typed in box...>
    
-         // var quantity = parseInt(inputText);
-   console.log("type", typeof inputText)
-   console.log("type", typeof quantity);
+   // var inputElement = parseInt(inputText);
+         // ^^^moving this line up as well
+         // getting message that quantity is not defined, adding a line below to see what happens.
+         var quantity = inputText
+   // console.log("type", typeof inputText)
+   // console.log("type", typeof quantity);
          // console.log("type", typeof quantity);
          // ^^^works to here...returns calcPrice and<input id="item1Quantity" type="text" value="0" and type string, plus input text 0 or what is typed in box and if text is in the box it returns NaN so parseInt works....>
   
          // console.log("type", typeOf quantity);
          // ^^^uncommenting this line causes error onclick in html calculatePrice nad that typeOf is not defined but returns all of the information above as well...WHY??? WHY>>>because it had a capitol O instead of a lower case o in typeof....LORD HELP ME!!!!! Found the error by copy/paste underneath the working line....
+
+   if (isNaN(quantity)) {
+      console.log("Only numbers please...")
+      return;
+   }
 }
-
-
-
+// It is not logging correctly. Keeps telling me that "only numbers please" no matter what I put in the box...changed the input of the html button to number to see what happens....
 // *******   ignore all of this stuff   ****************
 
 
